fac84df1c6207becf5ec37f8c542571026c7d157
miripom <37905291+miripom@users.noreply.github.com>
2025-08-18 12:20:01 +0200

    feat: added feature conta_parole

--- 18 Agosto/Esercizio 1/Esercizio1.py
+++ 18 Agosto/Esercizio 1/Esercizio1.py
@@ -1,3 +1,6 @@
+import Counter
+import re
+
 def conta_righe(file):
     try:
         with open(file, "r" , encoding="utf-8") as f:
@@ -6,4 +9,12 @@ def conta_righe(file):
         return f"Errore: il file '{file}' non esiste."
 
 
+def conta_parole(file):
+    try:
+        with open(file, "r" , encoding="utf-8") as f:
+            testo = f.read()
+            parole = re.findall(r"\w+", testo.lower())
+            return len(parole)
+    except FileNotFoundError:
+        return f"Errore: il file '{file}' non esiste."
 
