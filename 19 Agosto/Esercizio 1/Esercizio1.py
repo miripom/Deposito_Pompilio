7f4b89303cf18bfb8c371cc256040b4abd5ad9f8
miripom <37905291+miripom@users.noreply.github.com>
2025-08-19 10:57:12 +0200

    Aggiunta classificazione settimanale

--- 19 Agosto/Esercizio 1/Esercizio1.py
+++ 19 Agosto/Esercizio 1/Esercizio1.py
@@ -5,9 +5,13 @@ df = pd.read_csv("19 Agosto\Esercizio 1\PJME_hourly.csv", parse_dates=["Datetime
 print(df.head())
 
 df["Date"] = df["Datetime"].dt.date
+df["Week"] = df["Datetime"].dt.isocalendar().week
 
 df["Daily_Mean"] = df.groupby("Date")["PJME_MW"].transform("mean")
 
-df["Class"] = (df["PJME_MW"] > df["Daily_Mean"]).map({True:"Alto", False:"Basso"})
+df["Weekly_Mean"] = df.groupby("Week")["PJME_MW"].transform("mean")
+
+df["Daily_Class"] = (df["PJME_MW"] > df["Daily_Mean"]).map({True:"Alto", False:"Basso"})
+df["Weekly_Class"] = (df["PJME_MW"] > df["Weekly_Mean"]).map({True: "Alto", False:"Basso"})
 
 print(df.head())
