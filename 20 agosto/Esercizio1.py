4182ab7b4d3ab6dbdccabdd1c7a05377e8b41cfe
miripom <37905291+miripom@users.noreply.github.com>
2025-08-20 11:16:29 +0200

    Visualizzazione grafico

--- 20 agosto/Esercizio1.py
+++ 20 agosto/Esercizio1.py
@@ -8,5 +8,21 @@ print(df.head())
 
 X = df[["Annual Income (k$)", "Spending Score (1-100)"]]
 
-kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
+kmeans = KMeans(n_clusters=5, random_state=42, n_init=10)
 df["Cluster"] = kmeans.fit_predict(X)
+
+plt.figure(figsize=(8,6))
+for cluster in df["Cluster"].unique():
+    cluster_data = df[df["Cluster"] == cluster]
+    plt.scatter(
+        cluster_data["Annual Income (k$)"],
+        cluster_data["Spending Score (1-100)"],
+        s=50,
+        label = f"Cluster {cluster}"
+    )
+
+plt.xlabel("Annual Income (k$)")
+plt.ylabel("Spending Score (1-100)")
+plt.title("Cluster di clienti con K-Means (k=5)")
+plt.legend()
+plt.show()
